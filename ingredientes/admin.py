74798c2134390a5dcf0994caabbe371dd4968fb5
Maria Fernanda Mendoza <mendozamafer2000@mesoamericana.edu.gt>
2020-11-23 02:17:05 -0600

    Modelos, configuracion de admin y settings

--- ingredientes/admin.py
+++ ingredientes/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from ingredientes.models import Ingrediente, Receta
 
-# Register your models here.
+admin.site.register(Ingrediente)
+admin.site.register(Receta)
--- ingredientes/models.py
+++ ingredientes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Ingrediente(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nombre
+
+class Receta(models.Model):
+    titulo = models.CharField(max_length=100)
+    preparacion = models.TextField()
+    ingrediente = models.ForeignKey(
+    Ingrediente, related_name="ingredientes", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.titulo
